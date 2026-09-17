c3b100759a6d4ba67a45e830e68ddc092cf2ca0a
Jesús Franco <jesusfrancohueso@gmail.com>
2026-09-17 21:41:39 +0200

    Test 02

--- veterinario_core/settings.py
+++ veterinario_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b1$(j91^k(n^0uk0o(1mqqauf0%b0ri!x!(mvx)7nn&^rzc)&j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["veterinario-wheat.vercel.app", "127.0.0.1"]
 
 
 # Application definition

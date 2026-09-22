f33ff648b4d1340bfe625311a5a0668f2290f384
Jesús Franco <jesusfrancohueso@gmail.com>
2026-09-22 20:42:40 +0200

    Vercel allowed host 3

--- veterinario_core/settings.py
+++ veterinario_core/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-b1$(j91^k(n^0uk0o(1mqqauf0%b0ri!x!(mvx)7nn&^rzc)&j
 DEBUG = True
 
 ALLOWED_HOSTS = ["  .veterinario-wheat.vercel.app", 
+                    "veterinario-wheat.vercel.app",
                     "veterinario-5n7phyhdg-veterinario1.vercel.app",
                     ".veterinario-5n7phyhdg-veterinario1.vercel.app",
                     "127.0.0.1"
